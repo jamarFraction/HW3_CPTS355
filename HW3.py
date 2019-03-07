39ac3148cb01bd448059afd802d3bbc99b75324b
Jamar <jamar.fraction@gmail.com>
2019-03-06 19:46:51 -0800

    more assignment progress
    searchDicts, testsearchDicts2

--- HW3.py
+++ HW3.py
@@ -134,22 +134,112 @@ def testaddDictN():
     # Test Input
     testInput1 = [{'Mon':{'355':2,'360':2},'Tue':{'451':2,'360':3},'Thu':{'360':3}, 'Fri':{'355':2}, 'Sun':{'355':1}}, {'Tue':{'360':2},'Wed':{'355':2},'Fri':{'360':3, '355':1}}, {'Mon':{'360':5},'Wed':{'451':4},'Thu':{'355':3},'Fri':{'360':6}, 'Sun':{'355':5}}]
     testInput2 = [{'Mon': {'355': 2, '360': 2, '479': 8}}, {'Mon': {'360': 2}}, {'Mon': {'350': 5}}]
-    testInput3 = []
 
     # Run Tests
     if addDictN(testInput1) != {'355': 16, '360': 24, '451': 6}:
         return False
     if addDictN(testInput2) != {'350': 5, '355': 2, '360': 4, '479': 8}:
         return False
-    if addDictN(testInput3) != {}:
-        return False
 
     # all tests passed!
     return True
 
+def searchDicts(L,k): 
+
+    revList = list(L)
+
+    revList.reverse()
+
+    for subList in revList:
+        
+        if subList.get(k, None) != None:
+
+            return subList[k]
+
+    return None
+
+def testsearchDicts(): 
+
+    # Test Input
+    testInput1 = [{"x": 1, "y": True, "z": "found"}, {"x": 2}, {"y": False}]
+    testInput2 = [{"t": 1, "e": "soup"}, {"z": "found"}, {"e": 80}, {"q": True, "t": 15}]
+
+    # Run Tests
+    if searchDicts(testInput1, "x") != 2:
+        return False
+    if searchDicts(testInput1,"y") != False:
+        return False
+    if searchDicts(testInput1,"z") != "found":
+        return False
+    if searchDicts(testInput1,"t") != None:
+        return False
+    if searchDicts(testInput2, "z") != "found":
+        return False
+    if searchDicts(testInput2, "e") != 80:
+        return False
+    if searchDicts(testInput2, "t") != 15:
+        return False
+    
+    # all tests passed!
+    return True
+
+def searchDicts2Helper(index, L, k):
+
+    valueFound = L[index][1].get(k, None)
+
+    if index == 0:
+        return valueFound
+
+    elif valueFound == None:
+        newIndex = L[index][0]
+        return searchDicts2Helper(newIndex, L, k)
+
+    else:
+        return L[index][1][k] 
+
+def searchDicts2(L,k):
+
+    valueFound = searchDicts2Helper(len(L) - 1, L, k)
+
+    return valueFound
+
+def testsearchDicts2(): 
+
+    testInput1 = [(0, {"x": 0, "y": True, "z": "zero"}),
+                  (0, {"x": 1}),
+                  (1, {"y": False}),
+                  (1, {"x": 3, "z": "three"}),
+                  (2, {})]
+
+    testInput2 = [(0, {"x": 0, "y": True, "z": "yikes"}),
+                  (0, {"z": 1}),
+                  (5, {"y": 77}),
+                  (0, {"d": 8, "q": "deez"}),
+                  (2, {}),
+                  (3, {"x": 3, "p": "three"}),
+                  (2, {"x": 3})]
+
+    if searchDicts2(testInput1, "x") != 1:
+        return False
+    if searchDicts2(testInput1, "y") != False:
+        return False
+    if searchDicts2(testInput1, "z") != "zero":
+        return False
+    if searchDicts2(testInput1, "t") != None:
+        return False
+    if searchDicts2(testInput2, "d") != 8:
+        return False
+    if searchDicts2(testInput2, "z") != "yikes":
+        return False
+    if searchDicts2(testInput2, "y") != 77:
+        return False
+    
+    return True
 
 
-testFunctions = {"busStops": testbusStops, "addDict": testaddDict, "addDictN": testaddDictN}
+testFunctions = {"busStops": testbusStops, "addDict": testaddDict,
+                "addDictN": testaddDictN, "searchDicts": testsearchDicts,
+                "searchDicts2": testsearchDicts2}
 
 # MAIN, not gucci
 if __name__ == '__main__':
